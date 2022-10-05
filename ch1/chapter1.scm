8a5446b6f37c192914d4abaa330445562685cc49
Nathan Barlow <nathanjbarlow@gmail.com>
2022-10-05 08:19:49 -0500

    Refactor exercise 1.3

--- ch1/chapter1.scm
+++ ch1/chapter1.scm
@@ -1,24 +1,29 @@
 #lang scheme
 (define (square x) (* x x))
+
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
+
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
+
 (define (abs x)
   (cond ((> x 0) x)
         ((= x 0) 0)
         ((< x 0) (- x))))
 ;Exercise 1.3
+(define (greater-than-both x y z)
+  (and (> x y) (> x z)))
+
+(define (square-sum-with-largest x y z)
+  (if (> y z)
+      (sum-of-squares x y)
+      (sum-of-squares x z)))
+
 (define (sum-of-largest-two x y z)
-  (cond ((and (> x y) (> x z))
-         (if (> y z)
-             (sum-of-squares x y)
-             (sum-of-squares x z)))
-        ((and (> y x) (> y z))
-         (if (> x z)
-             (sum-of-squares y x)
-             (sum-of-squares y z)))
-        ((and (> z x) (> z y))
-         (if (> x y)
-             (sum-of-squares z x)
-             (sum-of-squares z y)))))
+  (cond ((greater-than-both x y z) (square-sum-with-largest x y z))
+        ((greater-than-both y x z) (square-sum-with-largest y x z))
+        ((greater-than-both z x y) (square-sum-with-largest z x y))))
+
+(display "Exercise 1.3: (sum-of-largest-two 5 10 8) ")
+(sum-of-largest-two 5 10 8)
